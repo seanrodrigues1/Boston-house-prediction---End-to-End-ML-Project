2af7207380a1f08ffa81f2a480cf7d66aca07438
seanrodrigues1 <seanrodrigues12@gmail.com>
2023-04-02 19:39:05 -0400

    this is our first commit and pushes a few important files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Boston-house-prediction---End-to-End-ML-Project
 Complete end to end ML project using flask web frame work, docker, ci/cd pipelines and delpoyment on Heroku cloud
+
+
+STEPS: 
+
+1. CREATE A NEW REPOSITORY - EG. BOSTON HOUSE PRICE PREDICTION
+2. CLONE REPOSITORY - create a local folder on your computer say "projects". copy the link of the repo on github , go to cmd , navigate to that local folder you created by cd path  and then type: git clone <github repo link>      note: you may have to install git CLI
---
+++ app.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 26 00:09:55 2023
+
+@author: seanr
+"""
+
+from flask import Flask, render_template, request
+#import jsonify
+import requests
+import pickle
+import numpy as np
+import sklearn
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__,template_folder='template_files')
+
+with open("xgb_model.pkl", "rb") as f:
+    xgb_model = pickle.load(f)
+    
+scalar=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('home.html')
+
+
+#standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
+
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=xgb_model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
+if __name__=="__main__":
+    app.run(debug=True)
